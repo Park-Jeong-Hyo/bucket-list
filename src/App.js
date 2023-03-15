45557860d802196ca67e1c4e93c57140bf53310c
ParkJeongHyo <qkrwjdgy123@naver.com>
2023-03-15 11:46:34 +0900

    수정기능추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import styled, {ThemeProvider} from 'styled-components/native'
-import theme from './theme'
-import Input from './components/Input'
+import styled, { ThemeProvider } from 'styled-components/native';
+import theme from './theme';
+import Input from './components/Input';
 import ListFormat from './components/ListFormat';
 import { useState } from 'react';
 
@@ -10,74 +10,80 @@ const Container = styled.SafeAreaView`
     flex:1;
     justify-content: flex-start;
     align-items:center;
-    background-color:${({theme}) => theme.mainBackground}
+    background-color:${({ theme }) => theme.mainBackground}
     gap: 30px;
-`
+`;
 const TitleText = styled.Text`
     font-size: 40px;
     font-weight:bold;
     letter-spacing: 10px;
-    background-color:${({theme}) => theme.textBackground}
+    background-color:${({ theme }) => theme.textBackground}
     margin: 0px 20px;
     border-radius:20px;
     text-align: center;
-    width:${({width}) => width - 40}px;
-`
+    width:${({ width }) => width - 40}px;
+`;
 
 function App() {
-    const width = Dimensions.get('window').width;
-    const [newInput, setNewInput] = useState('');
-    const [row, setRow] = useState({});
+  const width = Dimensions.get('window').width;
+  const [newInput, setNewInput] = useState('');
+  const [row, setRow] = useState({});
 
-    const addList = () => {
-        const ID = Date.now().toString();
-        const newInputObject = {
-            [ID] : {id: ID, text: newInput, completed: false}
-        }
-        setRow({...row, ...newInputObject})
-        setNewInput('');
-        console.log(row);
-    }
+  const addList = () => {
+    const ID = Date.now().toString();
+    const newInputObject = {
+      [ID]: { id: ID, text: newInput, completed: false },
+    };
+    setRow({ ...row, ...newInputObject });
+    setNewInput('');
+    console.log(row);
+  };
 
-    const changeInput = text => {
-        setNewInput(text)
-        console.log(`현재 입력된 텍스트: ${newInput}`)
-    }
+  const changeInput = text => {
+    setNewInput(text);
+    console.log(`현재 입력된 텍스트: ${newInput}`);
+  };
 
-    const _deleteList = id => {
-        const copyList = {...row};
-        delete copyList[id];
-        setRow(copyList);
-    }
-    const _toggleList = id => {
-        const copyList = {...row};
-        copyList[id]["completed"] = !copyList[id]["completed"];
-        setRow(copyList);
-    }
+  const _deleteList = id => {
+    const copyList = { ...row };
+    delete copyList[id];
+    setRow(copyList);
+  };
+  const _toggleList = id => {
+    const copyList = { ...row };
+    copyList[id]['completed'] = !copyList[id]['completed'];
+    setRow(copyList);
+  };
+  const _updateList = item => {
+    const copyList = Object.assign({}, row);
+    copyList[item.id] = item;
+    setRow(copyList);
+  };
 
   return (
     <ThemeProvider theme={theme}>
-        <Container theme={theme}>
-            <TitleText width={width}>버킷리스트</TitleText>
-            <Input 
-                value={newInput}
-                placeholder="+ add here"
-                theme={theme}
-                onSubmitEditing={addList}
-                onChangeText={changeInput}
+      <Container theme={theme}>
+        <TitleText width={width}>버킷리스트</TitleText>
+        <Input
+          value={newInput}
+          placeholder="+ add here"
+          theme={theme}
+          onSubmitEditing={addList}
+          onChangeText={changeInput}
+        />
+        {Object.values(row)
+          .reverse()
+          .map(item => (
+            <ListFormat
+              key={item.id}
+              text={item.text}
+              item={item}
+              deleteList={_deleteList}
+              toggleList={_toggleList}
+              updateList={_updateList}
             />
-            {Object.values(row)
-                .reverse()
-                .map( item => (
-                <ListFormat 
-                    key={item.id} 
-                    text={item.text}
-                    item={item}
-                    deleteList={_deleteList}
-                    toggleList={_toggleList}
-                />
-            ))}
-        </Container>
+          ))}
+      </Container>
     </ThemeProvider>
   );
 }
--- src/components/ListFormat.js
+++ src/components/ListFormat.js
@@ -1,33 +1,66 @@
-import React from "react";
-import Icons from "./Icons"
-import {Dimensions, Text} from "react-native";
-import styled from "styled-components/native";
-import images from "../images";
+import React, { useState } from 'react';
+import Icons from './Icons';
+import { Dimensions, Text } from 'react-native';
+import styled from 'styled-components/native';
+import images from '../images';
+import Input from './Input';
 
 const Container = styled.View`
     display:flex;
     flex-direction: row;
-    background-color: ${({theme})=> theme.textBackground}
-    width: ${({width}) => width - 40}px;
+    background-color: ${({ theme }) => theme.textBackground}
+    width: ${({ width }) => width - 40}px;
     gap: 10px;
-`
+`;
 const Content = styled.Text`
     flex: 1;
     font-size:40px;
-    color: ${({completed, theme}) => completed ? theme.textDone : theme.textColor}
-    text-decoration: ${({completed}) => completed ? 'line-through' : 'none'}
-`
+    color: ${({ completed, theme }) =>
+      completed ? theme.textDone : theme.textColor}
+    text-decoration: ${({ completed }) => (completed ? 'line-through' : 'none')}
+`;
 
-const ListFormat = ({text, item, deleteList, toggleList}) => {
-    const width = Dimensions.get('window').width
-    return (
-        <Container width={width}>
-            <Icons type={item.completed ? images.CheckBox : images.CheckBoxOutline} id={item.id} onPressOut={toggleList} completed={item.completed}/>
-            <Content completed={item.completed}>{text}</Content>
-            {item.completed || <Icons type={images.Edit}/>}
-            <Icons type={images.Delete} id={item.id} onPressOut={deleteList} completed={item.completed}/>
-        </Container>
-    );
-}
+const ListFormat = ({ item, deleteList, toggleList, updateList }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [text, setText] = useState(item.text);
+  const width = Dimensions.get('window').width;
+
+  const _editIconPressed = () => {
+    setIsEditing(true);
+  };
+  const _onSubmitEditing = () => {
+    if (isEditing) {
+      const editList = Object.assign({}, item, { text });
+      setIsEditing(false);
+      updateList(editList);
+    }
+  };
+  return isEditing ? (
+    <Input
+      value={text}
+      onChangeText={text => setText(text)}
+      onSubmitEditing={_onSubmitEditing}
+    />
+  ) : (
+    <Container width={width}>
+      <Icons
+        type={item.completed ? images.CheckBox : images.CheckBoxOutline}
+        id={item.id}
+        onPressOut={toggleList}
+        completed={item.completed}
+      />
+      <Content completed={item.completed}>{item.text}</Content>
+      {item.completed || (
+        <Icons type={images.Edit} onPressOut={_editIconPressed} />
+      )}
+      <Icons
+        type={images.Delete}
+        id={item.id}
+        onPressOut={deleteList}
+        completed={item.completed}
+      />
+    </Container>
+  );
+};
 
 export default ListFormat;
